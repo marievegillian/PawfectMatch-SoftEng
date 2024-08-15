811f7de3f6842c0686e1b98a6cca30435e2d9b16
Lyra Joyce G. Versoza <lyrajoyce.versoza@cit.edu>
2024-08-15 09:34:26 +0800

    Modify test input

--- pawfectmatch/flask_app/testAI.py
+++ pawfectmatch/flask_app/testAI.py
@@ -2,7 +2,7 @@ import requests
 
 url = "http://127.0.0.1:5000/filter_dogs"
 data = {
-    "user_input": "I want a loyal social strong-willed cheerful labrador dog"
+    "user_input": "I want a small and smart chihuahua dog"
 }
 response = requests.post(url, json=data)
 
